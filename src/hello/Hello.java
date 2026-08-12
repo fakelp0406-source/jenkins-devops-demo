cfc00490b2efaeb98245b242e6e535d0b29830ac
fakelp0406-source <fakelp0406@gmail.com>
2026-08-12 15:04:56 +0530

    updated Hello.java

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Pipeline CI/CD!");
+        System.out.println("Hello from Poll SCM trigger!");
     }
 }
